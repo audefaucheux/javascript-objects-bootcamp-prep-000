dcfeb615d0d3c070ce25e17ee39c590c03d406c5
Aude Faucheux <aude53@gmail.com>
2019-07-02 18:58:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,13 +1,11 @@
-var playlist = {
-  'Slowdive': 'Alison',
-  'My Bloody Valentine': 'Sometimes'
+var playlist = {artistName: 'songTitle'
 };
 
 function updatePlaylist (playlist, artistName, songTitle) {
   var playlist2 = artistName = "songTitle";
   return Object.assign(playlist, playlist2)
 
-function removeFromPlaylist(playlist,artistName) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  return playlist.artistName
+  return playlist
 }
