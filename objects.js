37311fe3d1ec964748967efe9e13674bdd3cf955
Aude Faucheux <aude53@gmail.com>
2019-07-02 18:45:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,5 @@ function updatePlaylist (playlist, artistName, songTitle) {
   playlist.artistName = songTitle;
   return playlist.artistName
 }
+
+updatePlaylist (playlist, 'Phil Ochs',"Here's to the State of Mississippi")
