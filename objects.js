fc88d1ad277534cf0d3fe03098a6a39c670e344a
Aude Faucheux <aude53@gmail.com>
2019-07-02 18:49:12 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist.artistName = songTitle;
-  return playlist.artistName
+  return playlist
 }
 
 function removeFromPlaylist(playlist,artistName) {
